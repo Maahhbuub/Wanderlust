448865851d5d7a27f3eb463fc334a83e752d61c4
Mahbub <mrahman221441@gmail.com>
2026-09-05 15:58:54 +0600

    fix(build): resolve build issue

--- app.js
+++ app.js
@@ -109,7 +109,8 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on: http://localhost:${PORT}/listings`);
-})
+});
